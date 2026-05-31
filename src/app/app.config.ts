ff625607c91f5ab396d36dde52b1761e3af84190
vikas <vp2841144@gmail.com>
2026-05-31 14:16:47 +0530

    adding ngrx library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(),
+    provideEffects(),
+    provideRouterStore(),
+  ],
 };
