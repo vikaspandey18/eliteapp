ff9ea83da83dd757a2d15845367e5430eff2ef9b
vikas <vp2841144@gmail.com>
2026-05-31 22:27:07 +0530

    Adding effect to fetch the customer

--- src/app/pages/customer/state/customer.effects.ts
+++ src/app/pages/customer/state/customer.effects.ts
@@ -1,7 +1,27 @@
 import { inject, Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Customer } from '../services/customer';
+import { loadCustomers, loadCustomersFailure, loadCustomersSuccess } from './customer.actions';
+import { catchError, map, of, switchMap } from 'rxjs';
 
 @Injectable()
 export class CustomerEffect {
   private actions$ = inject(Actions);
+  private customerService = inject(Customer);
+
+  loadCustomer$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(loadCustomers),
+      switchMap((action) => {
+        return this.customerService.getCustomer().pipe(
+          map((response) => {
+            return loadCustomersSuccess({ customers: response });
+          }),
+          catchError((err) => {
+            return of(loadCustomersFailure({ error: err?.error?.message || err?.message }));
+          }),
+        );
+      }),
+    );
+  });
 }
