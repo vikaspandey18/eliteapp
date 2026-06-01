e9a940cbe5ad2fbe2345fe8eab685dd291d5f9a3
vikaspande18 <vp2841144@gmail.com>
2026-06-01 11:33:04 +0530

    Fetching the use successfully

--- src/app/pages/customer/customer.ts
+++ src/app/pages/customer/customer.ts
@@ -14,6 +14,7 @@ import {
   selectCustomerMessage,
   selectCustomers,
 } from './state/customer.selectors';
+import { loadCustomers } from './state/customer.actions';
 
 @Component({
   selector: 'app-customer',
@@ -37,6 +38,9 @@ export class Customer implements OnInit {
   private searchSubject = new BehaviorSubject<string>('');
 
   ngOnInit(): void {
+
+    this.store.dispatch(loadCustomers());
+
     this.customer$ = this.store.select(selectCustomers);
 
     this.loading$ = this.store.select(selectCustomerLoading);
@@ -73,10 +77,7 @@ export class Customer implements OnInit {
   }
 
   getInitials(name: string): string {
-    return name
-      ?.split(' ')
-      .map((n) => n[0])
-      .join('')
-      .toUpperCase();
+    return name?.substring(0,2).toUpperCase() || "UN";
   }
+
 }
