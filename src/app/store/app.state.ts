04ad455834bb52bc0f96531abe3e2a3e2bef38e6
vikas <vp2841144@gmail.com>
2026-06-02 06:10:06 +0530

    Adding planner to global state

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,13 +1,18 @@
 import { CustomerEffect } from '../pages/customer/state/customer.effects';
 import { customerReducer } from '../pages/customer/state/customer.reducer';
 import { CustomerState } from '../pages/customer/state/customer.state';
+import { PlannerEffect } from '../pages/planner/state/planner.effects';
+import { plannerReducer } from '../pages/planner/state/planner.reducer';
+import { PlannerState } from '../pages/planner/state/planner.state';
 
 export interface AppState {
   customer: CustomerState;
+  planner: PlannerState;
 }
 
 export const AppReducer = {
   customer: customerReducer,
+  planner: plannerReducer,
 };
 
-export const AppEffect = [CustomerEffect];
+export const AppEffect = [CustomerEffect, PlannerEffect];
