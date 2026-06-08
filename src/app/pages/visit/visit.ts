aaed8860da62f130a93a1c2051f1249d1fe6aa95
vikaspande18 <vp2841144@gmail.com>
2026-06-08 18:52:05 +0530

    Updating the visit form

--- src/app/pages/visit/visit.ts
+++ src/app/pages/visit/visit.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { VisitService } from './service/visit-service';
+import { finalize, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-visit',
@@ -9,6 +11,14 @@ import { RouterLink } from '@angular/router';
   styleUrl: './visit.css',
 })
 export class Visit {
+  private visitService = inject(VisitService);
+
+  private subscription = new Subscription();
+
+  loading = false;
+
+  error: string | null = null;
+
   visitForm = new FormGroup({
     followUpDate: new FormControl(''),
     comment: new FormControl('', Validators.required),
@@ -32,11 +42,51 @@ export class Visit {
   }
 
   onSubmit() {
-    if (this.visitForm.valid) {
-      console.log('Form submitted:', this.visitForm.value);
-      alert('Visit form submitted successfully!');
-    } else {
+    if (this.visitForm.invalid) {
       this.visitForm.markAllAsTouched();
+      return;
     }
+
+    this.loading = true;
+
+    this.error = null;
+
+    const formData = new FormData();
+
+    formData.append('followUpDate', this.visitForm.value.followUpDate ?? '');
+
+    formData.append('comment', this.visitForm.value.comment ?? '');
+
+    formData.append('purpose', this.visitForm.value.purpose ?? '');
+
+    if (this.visitForm.value.photo) {
+      formData.append('photo', this.visitForm.value.photo);
+    }
+
+    const sub = this.visitService
+      .addVisit(formData)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        }),
+      )
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          alert('Saved Successfully');
+          this.visitForm.reset();
+          this.photoPreview = null;
+        },
+        error: (err) => {
+          console.log(err);
+          this.error = 'Failed to save visit';
+        },
+      });
+
+    this.subscription.add(sub);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
