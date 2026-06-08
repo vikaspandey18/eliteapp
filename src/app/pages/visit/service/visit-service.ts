a508ecbc33240a1e866e76acd01565a14bb05361
vikaspande18 <vp2841144@gmail.com>
2026-06-08 18:15:09 +0530

    Adding the addvisit function in the visitservices

--- src/app/pages/visit/service/visit-service.ts
+++ src/app/pages/visit/service/visit-service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { ApiResponse } from '../../../models/api.response.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class VisitService {}
+export class VisitService {
+  private http = inject(HttpClient);
+
+  addVisit(formData: FormData): Observable<ApiResponse<null>> {
+    const url = `${environment.apiUrl}/visit/addVisit.php`;
+    return this.http.post<ApiResponse<null>>(url, formData);
+  }
+}
