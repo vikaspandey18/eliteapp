b3575e09e1612ede456be7fbcdca60483d7c4bed
vikaspande18 <vp2841144@gmail.com>
2026-06-08 17:52:16 +0530

    Fetching the single customer for the planner menu

--- src/app/pages/planner/planner-menu/planner-menu.ts
+++ src/app/pages/planner/planner-menu/planner-menu.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CustomerModel } from '../../../models/customer.model';
+import { Store } from '@ngrx/store';
+import { selectCustomerFromRoute } from '../../customer/state/customer.selectors';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-planner-menu',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './planner-menu.html',
   styleUrl: './planner-menu.css',
 })
-export class PlannerMenu {}
+export class PlannerMenu implements OnInit {
+  private store = inject(Store);
+
+  customer$!: Observable<CustomerModel | undefined>;
+
+  ngOnInit(): void {
+    this.customer$ = this.store.select(selectCustomerFromRoute);
+  }
+}
