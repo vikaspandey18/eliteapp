83496c8fce30cebb6f6119aeef92c86391b0d438
vikaspande18 <vp2841144@gmail.com>
2026-06-09 12:09:40 +0530

    config: Register Visit and Collection states in AppState

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -6,16 +6,28 @@ import { PlannerEffect } from '../pages/planner/state/planner.effects';
 import { plannerReducer } from '../pages/planner/state/planner.reducer';
 import { PlannerState } from '../pages/planner/state/planner.state';
 
+import { VisitEffect } from '../pages/visit/state/visit.effects';
+import { visitReducer } from '../pages/visit/state/visit.reducer';
+import { VisitState } from '../pages/visit/state/visit.state';
+
+import { CollectionEffect } from '../pages/collection/state/collection.effects';
+import { collectionReducer } from '../pages/collection/state/collection.reducer';
+import { CollectionState } from '../pages/collection/state/collection.state';
+
 export interface AppState {
   customer: CustomerState;
   planner: PlannerState;
+  visit: VisitState;
+  collection: CollectionState;
   router: RouterReducerState;
 }
 
 export const AppReducer = {
   customer: customerReducer,
   planner: plannerReducer,
+  visit: visitReducer,
+  collection: collectionReducer,
   router: routerReducer,
 };
 
-export const AppEffect = [CustomerEffect, PlannerEffect];
+export const AppEffect = [CustomerEffect, PlannerEffect, VisitEffect, CollectionEffect];
