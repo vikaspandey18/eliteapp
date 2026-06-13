6550de0cd0a14edcfb6d768cc9152fe5c4f49cbf
vikaspande18 <vp2841144@gmail.com>
2026-06-13 11:45:11 +0530

    adding the all ativity icon

---
+++ src/app/pages/allactivity/allactivity.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allactivity',
+  imports: [],
+  templateUrl: './allactivity.html',
+  styleUrl: './allactivity.css',
+})
+export class Allactivity {}
