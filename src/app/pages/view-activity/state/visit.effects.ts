06fee3b538b9cb7d113e16dc54958c8c5b70307f
vikas <vp2841144@gmail.com>
2026-06-28 15:36:39 +0530

    Updated the visit effect and add the html view also

--- src/app/pages/view-activity/state/visit.effects.ts
+++ src/app/pages/view-activity/state/visit.effects.ts
@@ -2,12 +2,27 @@ import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { ViewVisit } from '../services/view-visit';
 import { loadVisitDetail, loadVisitDetailFailed, loadVisitDetailSuccess } from './visit.actions';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, filter, map, of, switchMap } from 'rxjs';
+import { routerNavigationAction } from '@ngrx/router-store';
 
 @Injectable()
 export class VisitDetailEffect {
   private actions$ = inject(Actions);
   private visitDetailService = inject(ViewVisit);
+
+  loadVisitDetailOnNavigation$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(routerNavigationAction),
+      filter((action: any) => {
+        const url = action.payload.routerState.url;
+        return url.startsWith('/viewactivity/');
+      }),
+      map((action: any) => {
+        const id = action.payload.routerState.params.id;
+        return loadVisitDetail({ id });
+      }),
+    );
+  });
 
   loadVisitDetail$ = createEffect(() => {
     return this.actions$.pipe(
@@ -25,3 +40,6 @@ export class VisitDetailEffect {
     );
   });
 }
+
+
+
--- src/app/pages/view-activity/view-activity.ts
+++ src/app/pages/view-activity/view-activity.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, CommonModule, DatePipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { VisitDetailModel } from '../../models/visit-detail.model';
+import { selectVisitDetail, selectVisitDetailError, selectVisitDetailLoading } from './state/visit.selectors';
 
 @Component({
   selector: 'app-view-activity',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe, DatePipe, CommonModule],
   templateUrl: './view-activity.html',
   styleUrl: './view-activity.css',
 })
-export class ViewActivity {}
+export class ViewActivity implements OnInit {
+  private store = inject(Store);
+
+  visitDetail$!: Observable<VisitDetailModel | null>;
+  loading$!: Observable<boolean>;
+  error$!: Observable<string | null>;
+
+  ngOnInit(): void {
+    this.visitDetail$ = this.store.select(selectVisitDetail);
+    this.loading$ = this.store.select(selectVisitDetailLoading);
+    this.error$ = this.store.select(selectVisitDetailError);
+  }
+}
+
