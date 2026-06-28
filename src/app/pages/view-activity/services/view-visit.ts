4888622b062a38e496e5850c1ffd8f64e31713f6
vikas <vp2841144@gmail.com>
2026-06-28 13:01:26 +0530

    adding the service code to fetch data for the visit and collection

--- src/app/pages/view-activity/services/view-visit.ts
+++ src/app/pages/view-activity/services/view-visit.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../../../models/api.response.model';
+import { VisitDetailModel } from '../../../models/visit-detail.model';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ViewVisit {}
+export class ViewVisit {
+  private http = inject(HttpClient);
+
+  getVisitDetail(id: string): Observable<ApiResponse<VisitDetailModel>> {
+    const url = `${environment.apiUrl}/activity/visitDetail.php`;
+    return this.http.post<ApiResponse<VisitDetailModel>>(url, { id });
+  }
+}
